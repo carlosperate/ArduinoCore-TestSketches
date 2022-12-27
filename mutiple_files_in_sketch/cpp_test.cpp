a98cc904c440a212d0f4a6b06713e923ba1acd86
Carlos Pereira Atencio <carlosperate@embeddedlog.com>
2022-12-27 01:11:03 +0000

    Fix sketch with extra files in subfolder.
    The Arduino IDE is only meant to look for additional files inside
    a "src" folder.

--- mutiple_files_in_sketch/cpp_test.cpp
+++ mutiple_files_in_sketch/cpp_test.cpp
@@ -1,5 +1,5 @@
 #include "header_top_dir.h"
-#include "subfolder/header_subfolder.h"
+#include "src/header_subfolder.h"
 
 int multiply(int input) {
     return input * get_mutiplier();
--- mutiple_files_in_sketch/mutiple_files_in_sketch.ino
+++ mutiple_files_in_sketch/mutiple_files_in_sketch.ino
@@ -1,16 +1,16 @@
 #include "header_top_dir.h"
 
 void setup() {
-  Serial.begin(115200);
-  while (!Serial);
-  Serial.println(func_without_declaration(2));
+    Serial.begin(115200);
+    while (!Serial) continue;
+    Serial.println(func_without_declaration(2));
 }
 
 int func_without_declaration(int numb) {
-  // This function will multiply by 3
-  return multiply(numb);
+    // This function will multiply by 3
+    return multiply(numb);
 }
 
 void loop() {
-  delay(1000);
+    delay(1000);
 }
